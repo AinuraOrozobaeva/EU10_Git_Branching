8211d002a5713bc7ef7af12b9e7242a5ef291939
44757 <asinoori555@gmail.com>
2022-12-10 13:57:13 +0000

    added positive testing for TC001

--- src/TestCases/TC001.java
+++ src/TestCases/TC001.java
@@ -7,5 +7,6 @@ public class TC001 {
         System.out.println("Testing TC001");
         System.out.println("More testing TC001");
         System.out.println("Negative testing for TC001");
+        System.out.println("Positive testing for TC001");
     }
 }
